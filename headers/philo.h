053f75ceaa6092317999378eec91b70ea80b76c5
mrmoustach <bossissamos@gmail.com>
2021-06-27 20:59:11 +0100

    started experimenting

---
+++ headers/philo.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zed <zed@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/27 17:01:39 by zed               #+#    #+#             */
+/*   Updated: 2021/06/27 17:04:14 by zed              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+typedef struct s_philo
+{
+    
+}               t_philo;
+
+#endif
---
+++ main.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zed <zed@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/27 16:24:55 by zed               #+#    #+#             */
+/*   Updated: 2021/06/27 17:03:28 by zed              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "headers/philo.h"
+
+pthread_t tid[2];
+int counter;
+pthread_mutex_t lock;
+
+
+int test()
+{
+	pthread_mutex_lock(&lock);
+	counter += 1;
+	printf("\n Job %d has started\n", counter);
+    printf("lmao %d", counter);    
+	pthread_mutex_unlock(&lock);
+}
+
+void* trythis(void* arg)
+{
+
+	unsigned long i = 0;
+	sleep(2);
+    test();
+
+	printf("\n Job %d has finished\n", counter);
+
+
+	return NULL;
+}
+
+int main(void)
+{
+	int i = 0;
+	int error;
+
+	if (pthread_mutex_init(&lock, NULL) != 0) {
+		printf("\n mutex init has failed\n");
+		return 1;
+	}
+
+	while (i < 2) {
+		error = pthread_create(&(tid[i]),
+							NULL,
+							&trythis, NULL);
+		if (error != 0)
+			printf("\nThread can't be created :[%s]",
+				strerror(error));
+		i++;
+	}
+
+	pthread_join(tid[0], NULL);
+	pthread_join(tid[1], NULL);
+	pthread_mutex_destroy(&lock);
+
+	return 0;
+}
